41995e6dd7aaf417387a4825af9402d29b93d3b8
Ron Dollete <ron@globalfitnessmedia.com>
2011-04-16 09:45:59 -0700

    Removing copy_move

--- vendor/extensions/copy_move/config/routes.rb
+++
@@ -1,8 +0,0 @@
-ActionController::Routing::Routes.draw do |map|
-  map.with_options(:controller => "admin/pages") do |page|
-    page.copy_page     '/admin/pages/:id/copy_page',     :action => 'copy_page'
-    page.copy_children '/admin/pages/:id/copy_children', :action => 'copy_children'
-    page.copy_tree     '/admin/pages/:id/copy_tree',     :action => 'copy_tree'
-    page.move_page     '/admin/pages/:id/move_page',     :action => 'move_page'
-  end
-end
--- vendor/extensions/copy_move/lib/copy_move.rb
+++
@@ -1,8 +0,0 @@
-module CopyMove
-  class CircularHierarchy < ActiveRecord::ActiveRecordError
-    def initialize(record)
-      @record = record
-      super("Page #{record.title} cannot be made a descendant of itself.")
-    end
-  end
-end
